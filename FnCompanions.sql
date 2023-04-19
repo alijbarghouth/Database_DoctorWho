6c2e457fc0ed98268d986d88576b21fd1055bde4
alijbarghouth <alijbarghouth@gmail.com>
2023-04-19 15:13:51 +0300

    add Function

---
+++ FnCompanions.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION fnCompanions (
+	@id INT
+)
+RETURNS VARCHAR(MAX)
+BEGIN
+	DECLARE @return_value VARCHAR(MAX)
+	SELECT @return_value =  COALESCE(@return_value + ', ','') + CompanionName 
+	FROM tblCompanion C JOIN tblEpisodeCompanion E 
+	ON C.CompanionId = E.CompanionId
+	WHERE E.EpisodeId = @id;
+ 
+    RETURN @return_value
+END;
