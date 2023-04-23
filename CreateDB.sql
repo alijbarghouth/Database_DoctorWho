860c15269fc1ac2c95eb3e94999732d3aa09e1a9
alijbarghouth <alijbarghouth@gmail.com>
2023-04-23 22:42:56 +0300

    create Database

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,3 +1,15 @@
-DROP DATABASE IF EXISTS DoctorWho;
 
-CREATE DATABASE DoctorWho;
+IF NOT EXISTS (
+    SELECT
+        1
+    FROM
+        sys.databases
+    WHERE
+        [name] = 'DoctorWho'
+)
+
+BEGIN
+
+    CREATE DATABASE DoctorWho;
+
+END
