042b6104aa0d916b3eb593324638bca0d99a43da
alijbarghouth <alijbarghouth@gmail.com>
2023-04-23 22:59:18 +0300

    create Database

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,15 +1,13 @@
-
 IF NOT EXISTS (
     SELECT
-        1
-    FROM
-        sys.databases
-    WHERE
-        [name] = 'DoctorWho'
+    1
+  FROM
+    sys.databases
+  WHERE
+    [name] = 'DoctorWho'
+  OR
+    name = 'DoctorWho'
 )
-
 BEGIN
-
-    CREATE DATABASE DoctorWho;
-
+CREATE DATABASE DoctorWho;
 END
