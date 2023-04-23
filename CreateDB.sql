935909666c79ded5c2a319cac7556c6e2f761ea7
alijbarghouth <alijbarghouth@gmail.com>
2023-04-23 22:56:32 +0300

    create فشلامث

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,3 +1,14 @@
-DROP DATABASE IF EXISTS DoctorWho;
+IF NOT EXISTS (
+    SELECT
+        1
+    FROM
+        sys.databases
+    WHERE
+        [name] = 'DoctorWho'
+)
 
-CREATE DATABASE DoctorWho;
+BEGIN
+
+    CREATE DATABASE DoctorWho;
+
+END
